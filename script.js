69b10ed60ae1785a34e60b72b98f00a9c2a12cf9
yashpatel-2403 <yashpatel240303@gmail.com>
2025-12-24 19:08:22 +0530

    To-Do Task Addition Done

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const theme = document.getElementById("themr-switcher");
-  
+// -------------todo---------------
+
+const inp = document.getElementById("input");
+const addtask = document.getElementById("add-task");
+const list = document.getElementById("todo-list");
+
+
+function addTask() {
+    let input = inp.value.trim();
+
+
+    const li = document.createElement("li");
+    // const li = document.querySelector("li");
+    li.innerHTML = `<input type="checkbox">${input}<img src="assets/delete.png">`
+
+    list.appendChild(li);
+    inp.value = "";
+
+}
+
+addtask.addEventListener('click', addTask);
